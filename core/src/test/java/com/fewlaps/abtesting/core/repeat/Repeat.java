80bb72b2c2ddae94e50f56f0125fb0c274409fd7
Roc Boronat <roc@fewlaps.com>
2018-11-21 01:23:23 +0800

    Reproduce a bug that happens when using two AB tests at the same time

---
+++ core/src/test/java/com/fewlaps/abtesting/core/repeat/Repeat.java
@@ -0,0 +1,13 @@
+package com.fewlaps.abtesting.core.repeat;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention( RetentionPolicy.RUNTIME )
+@Target( { 
+  java.lang.annotation.ElementType.METHOD
+} ) 
+public @interface Repeat { 
+  public abstract int times(); 
+} 
--- core/src/test/kotlin/com/fewlaps/abtesting/core/ABTestingShould.kt
+++ core/src/test/kotlin/com/fewlaps/abtesting/core/ABTestingShould.kt
@@ -2,20 +2,31 @@ package com.fewlaps.abtesting.core
 
 import com.fewlaps.abtesting.core.doubles.ABTestingRepositoryEmpty
 import com.fewlaps.abtesting.core.doubles.ABTestingRepositoryFake
+import com.fewlaps.abtesting.core.repeat.Repeat
+import com.fewlaps.abtesting.core.repeat.RepeatRule
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.mockito.ArgumentMatchers.anyInt
 import org.mockito.BDDMockito.given
 import org.mockito.Mockito.mock
 
-val EXPERIMENT_NAME = "yes or no"
 val OPTION_YES = "yes"
 val OPTION_NO = "no"
-val YES_OR_NO_EXPERIMENT = Experiment(EXPERIMENT_NAME, listOf(OPTION_YES, OPTION_NO))
+val YES_OR_NO_EXPERIMENT_NAME = "yes or no"
+val YES_OR_NO_EXPERIMENT = Experiment(YES_OR_NO_EXPERIMENT_NAME, listOf(OPTION_YES, OPTION_NO))
+
+val OPTION_TRUE = "true"
+val OPTION_FALSE = "false"
+val TRUE_OR_FALSE_EXPERIMENT_NAME = "true or false"
+val TRUE_OR_FALSE_EXPERIMENT = Experiment(TRUE_OR_FALSE_EXPERIMENT_NAME, listOf(OPTION_TRUE, OPTION_FALSE))
 
 class ABTestingShould {
+
+    @Rule @JvmField
+    public val repeatRule: RepeatRule = RepeatRule()
 
     lateinit var randomGenerator: RandomGenerator
     lateinit var abTesting: ABTesting
@@ -23,14 +34,27 @@ class ABTestingShould {
     @Before
     fun setUp() {
         randomGenerator = mock(RandomGenerator::class.java)
-        abTesting = ABTesting(listOf(YES_OR_NO_EXPERIMENT), ABTestingRepositoryFake(), randomGenerator)
+        abTesting = ABTesting(
+                listOf(YES_OR_NO_EXPERIMENT, TRUE_OR_FALSE_EXPERIMENT),
+                ABTestingRepositoryFake(),
+                randomGenerator)
     }
 
     @Test
     fun returnListOfExperiments() {
         val experiments: List<Experiment> = abTesting.experiments
 
-        assertEquals(EXPERIMENT_NAME, experiments[0].name)
+        assertEquals(YES_OR_NO_EXPERIMENT_NAME, experiments[0].name)
+    }
+
+    @Test
+    @Repeat(times = 1000)
+    fun canWorkWithMultipleTests() {
+        abTesting = ABTesting(
+                listOf(YES_OR_NO_EXPERIMENT, TRUE_OR_FALSE_EXPERIMENT),
+                ABTestingRepositoryFake(),
+                RandomGenerator())
+        abTesting.getCurrentOptions()
     }
 
     @Test
@@ -38,15 +62,18 @@ class ABTestingShould {
         val currentOptions: List<CurrentOption> = abTesting.getCurrentOptions()
         given(randomGenerator.getRandom(anyInt())).willReturn(0)
 
-        assertEquals(EXPERIMENT_NAME, currentOptions[0].experiment)
+        assertEquals(YES_OR_NO_EXPERIMENT_NAME, currentOptions[0].experiment)
         assertEquals(OPTION_YES, currentOptions[0].option)
+
+        assertEquals(TRUE_OR_FALSE_EXPERIMENT_NAME, currentOptions[1].experiment)
+        assertEquals(OPTION_TRUE, currentOptions[1].option)
     }
 
     @Test
     fun returnDetailOfExperiment() {
-        val storedExperiment = abTesting.getExperiment(EXPERIMENT_NAME)
+        val storedExperiment = abTesting.getExperiment(YES_OR_NO_EXPERIMENT_NAME)
 
-        assertEquals(EXPERIMENT_NAME, storedExperiment.name)
+        assertEquals(YES_OR_NO_EXPERIMENT_NAME, storedExperiment.name)
         assertEquals(OPTION_YES, storedExperiment.options[0])
         assertEquals(OPTION_NO, storedExperiment.options[1])
     }
@@ -70,10 +97,10 @@ class ABTestingShould {
     @Test
     fun returnedOptionShouldBeAlwaysTheSame() {
         mockRandomToReturn(0)
-        val currentOption = abTesting.getCurrentOptionFor(EXPERIMENT_NAME)
+        val currentOption = abTesting.getCurrentOptionFor(YES_OR_NO_EXPERIMENT_NAME)
         mockRandomToReturn(1)
 
-        val newOption = abTesting.getCurrentOptionFor(EXPERIMENT_NAME)
+        val newOption = abTesting.getCurrentOptionFor(YES_OR_NO_EXPERIMENT_NAME)
 
         assertEquals(currentOption, newOption)
     }
@@ -85,7 +112,7 @@ class ABTestingShould {
         var noCount = 0;
 
         for (i in 1..100) {
-            when (abTesting.getCurrentOptionFor(EXPERIMENT_NAME)) {
+            when (abTesting.getCurrentOptionFor(YES_OR_NO_EXPERIMENT_NAME)) {
                 OPTION_YES -> yesCount++
                 OPTION_NO -> noCount++
             }
